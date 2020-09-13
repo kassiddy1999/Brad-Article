cd72ac99032cd8d87d85623ec77161cad9a1ba06
kassidy10 <victoriheanachoc@gmail.com>
2020-09-13 23:49:48 +0100

    making a commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+    {
+    'id':1,
+    'title':'Article One',
+    'body':'Lorem ipsum dolor sit amet, consectetur         adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    'author':'Brad Traversy',
+    'create_date':'04-23-2020'
+    },
+     {
+    'id':2,
+    'title':'Article Two',
+    'body':'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    'author':'Viktor Iheanacho',
+    'create_date':'03-21-2020'
+    },
+     {
+    'id':1,
+    'title':'Article Three',
+    'body':'Lorem ipsum dolor sit amet, consectetur   adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    'author':'Brad Traversy',
+    'create_date':'04-23-2020'
+    },
+    ]
+    return articles
